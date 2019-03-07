8087c52733185d8800b868509d7105bdc6d4e9bf
Anatoly <east@thyloved.ru>
2019-03-07 21:51:00 +0300

    Docker

---
+++ docker-example/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Start MySQL
+service mysql start
+mysql -u root -e "GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT, USAGE ON *.* TO 'debezium' IDENTIFIED BY 'dbz'";
+mysql -u root -e "CREATE database opencart; use opencart; CREATE TABLE oc_product(id int, name text)";
+
+sleep 2
+# Start Mongo
+cd /u/apps/mongodb-replicaset/01-simple-replset && bash stop-cluster.sh && bash start-cluster.sh && bash start-replication.sh
+mongo --port 27017 < '/mongo-create-user.js'
+
+
+# sudo docker build /u/apps/debezium-docker/ --tag=debezium --no-cache
+# sudo docker run debezium
+# 
+# sudo docker exec -it 7e3916df4bb6 /bin/bash && cd /u/apps/debezium-embedded/mongo && mvn exec:java
+# sudo docker exec -it 7e3916df4bb6 /bin/bash && cd /u/apps/debezium-embedded/mysql && mvn exec:java
+
